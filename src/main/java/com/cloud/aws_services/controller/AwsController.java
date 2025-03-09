106421332d043506c44f3b3da990b93687aad82c
Shivam Patel <shivampatelcodes01@gmail.com>
2025-03-09 16:42:34 -0400

    implement s3 upload method

--- src/main/java/com/cloud/aws_services/controller/AwsController.java
+++ src/main/java/com/cloud/aws_services/controller/AwsController.java
@@ -2,21 +2,43 @@ package com.cloud.aws_services.controller;
 
 import com.cloud.aws_services.service.S3Service;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/aws")
+@RequestMapping("/aws/s3")
 public class AwsController {
 
     private final S3Service s3Service;
 
-    @GetMapping("/s3/buckets")
+    @GetMapping("/buckets")
     public List<String> listBuckets(){
         return s3Service.listBuckets();
+    }
+
+    @GetMapping("/objects")
+    public ResponseEntity<List<String>> listObjects(@RequestParam String bucketName) {
+        List<String> objects = s3Service.listObjects(bucketName);
+        return ResponseEntity.ok(objects);
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> addFile(@RequestParam("bucketName") String bucketName,
+                                          @RequestParam("key") String key,
+                                          @RequestParam("file") MultipartFile file) {
+        try{
+            s3Service.uploadFile(bucketName, key, file);
+            return ResponseEntity.ok("File uploaded successfully.");
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error uploading file: " + e.getMessage());
+        }
     }
 }
--- src/main/java/com/cloud/aws_services/service/S3Service.java
+++ src/main/java/com/cloud/aws_services/service/S3Service.java
@@ -2,12 +2,12 @@ package com.cloud.aws_services.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.Bucket;
-import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.Path;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,11 +24,24 @@ public class S3Service {
 
     }
 
-    public void uploadFile(String bucketName, String key, Path filePath){
-        s3Client.putObject(PutObjectRequest.builder()
+    public List<String> listObjects(String bucketName) {
+        ListObjectsV2Request request = ListObjectsV2Request.builder()
                 .bucket(bucketName)
-                .key(key)
-                .build(), filePath);
+                .build();
+        ListObjectsV2Response response = s3Client.listObjectsV2(request);
+        return response.contents().stream()
+                .map(S3Object::key)
+                .collect(Collectors.toList());
+    }
+
+    public void uploadFile(String bucketName, String key, MultipartFile file) throws IOException {
+        s3Client.putObject(
+                PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(key)
+                        .build(),
+                RequestBody.fromInputStream(file.getInputStream(), file.getSize())
+        );
     }
 
 }
